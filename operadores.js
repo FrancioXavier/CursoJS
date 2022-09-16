883f5af23ac9145a0b028240f5894aefd1b5110d
FrancioXavier <103940118+FrancioXavier@users.noreply.github.com>
2022-09-16 14:47:16 -0300

    Aula 7

---
+++ operadores.js
@@ -0,0 +1,47 @@
+//OPERADORES ARITMÉTICOS
+
+// soma
+var num = 5 + 2
+ 
+
+// subtração
+5 - 2
+
+//multiplicação
+
+5 * 2
+
+// divisão
+
+5 / 2
+
+// resto de uma divisão 
+
+5 % 2
+
+// potência
+
+5 ** 2
+
+/* OBS.: O Javascript respeita a precedência de operadores */
+
+// OPERADOR DE ATRIBUIÇÃO
+
+// operadores comuns para guardar dados em variáveis
+
+var a = 5 + 3
+var b = a % 5
+
+// auto-atribuição
+
+var a2 = 1
+a2 = a + 1 // a == 2
+
+// OOOOU....
+
+a2 += 1 
+
+//OPERADORES DE INCREMENTO
+
+a2++ // == a2 += 1
+a2-- // == a2 -= 1
