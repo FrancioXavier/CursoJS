4f48ab0c11616f952b454de8f917be0904fb4c2c
FrancioXavier <103940118+FrancioXavier@users.noreply.github.com>
2022-09-17 17:59:30 -0300

    aula09

---
+++ Aula09/dom.js
@@ -0,0 +1,16 @@
+//o que é DOM?
+
+//Document Object Model:
+
+/*
+ÁRVORE DOM
+
+            __________1. window____________
+           |             |                |
+    1.1 location    1.2 document    1.3 history
+                        |
+                    ex.: HTML
+
+
+
+*/
