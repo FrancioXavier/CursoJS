9530aae018c2399b8fdd8767d94687ed17e4d778
FrancioXavier <103940118+FrancioXavier@users.noreply.github.com>
2022-11-23 00:50:06 -0300

    iterações

---
+++ Aula13e14/iterations.js
@@ -0,0 +1,27 @@
+//ITERAÇOES EM JAVASCRIPT
+
+
+/*WHILE
+
+Estrutura: 
+
+var i = 0;
+
+while(i < 10){
+    console.log("Batata");
+    i++;
+}*/
+
+//DO WHILE
+
+/*
+var i = 0;
+
+do{
+    console.log(`batata ${i+1}`);
+    i++;
+}while(i<10); */
+
+for(let i = 0; i < 10; i++){
+    console.log(`batata ${i+1}`)
+}
